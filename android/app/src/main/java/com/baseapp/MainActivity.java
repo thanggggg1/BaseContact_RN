462bd27eca5b6b2d2c3a5c3e418da814f812a7d1
Base <base@Bases-iMac.local>
2022-04-01 17:25:01 +0700

    updaet

--- android/app/src/main/java/com/baseapp/MainActivity.java
+++ android/app/src/main/java/com/baseapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.baseapp;
 import android.os.Bundle; // here
 import org.devio.rn.splashscreen.SplashScreen; // here
-import com.cboy.rn.splashscreen.SplashScreen; // here
+import org.devio.rn.splashscreen.SplashScreen;
 
 import com.facebook.react.ReactActivity;
 
